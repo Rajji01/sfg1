a8365faf06d9d7a16a511957c160c202d6e0e90c
Rajji02 <rajat.agrawal@letsgettin.com>
2019-09-30 16:33:52 +0530

    intial data

---
+++ src/test/java/com/example/sfgclinic/SfgClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.sfgclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SfgClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
